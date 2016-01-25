b9f5b5ec8aff818bb23e3dc1f31a86b425c6fb5e
Avrumi Friedman <info@lakewoodprogramming.com>
2016-01-25 13:08:17 -0500

    Cookies

---
+++ MvcApplication8/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int value = 0;
+            if (Request.Cookies["number"] != null)
+            {
+                value = int.Parse(Request.Cookies["number"].Value);
+            }
+
+            HttpCookie cookie = new HttpCookie("number", (value + 1).ToString());
+            Response.Cookies.Add(cookie);
+
+            return View(value);
+        }
+
+    }
+}
---
+++ MvcApplication8/Scripts/index.js
@@ -0,0 +1,12 @@
+﻿$(function() {
+    $("#clickme").on('click', function() {
+        var number = $("#override").val();
+        $.cookie('number', number);
+        window.location.reload();
+    });
+
+    $("#delete").on('click', function() {
+        $.removeCookie('number');
+        window.location.reload();
+    });
+})
